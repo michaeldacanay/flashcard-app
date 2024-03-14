b25ea7c45a917b2f3ba9a0cdfa6f3963d8024ee5
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-14 16:49:40 -0400

    Initial Commit

---
+++ Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Michael Dacanay on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
