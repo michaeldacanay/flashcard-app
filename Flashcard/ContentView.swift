cebd7619d631175b19cf1c1e1ef71c025c8199a2
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-14 23:12:39 -0400

    step 4: add deck of cards

--- Flashcard/ContentView.swift
+++ Flashcard/ContentView.swift
@@ -8,33 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var cards: [Card] = Card.mockedCards
+    
     var body: some View {
+        // Card deck
         ZStack {
-
-            // Card background
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(Color.blue.gradient)
-                .shadow(color: .black, radius: 4, x: -2, y: 2)
-
-            VStack(spacing: 20) {
-
-                // Card type (question vs answer)
-                Text("Question")
-                    .bold()
-                
-                // Separator
-                Rectangle()
-                    .frame(height: 1)
-
-                // Card text
-                Text("Located at the southern end of Puget Sound, what is the capitol of Washington?")
+            ForEach(0..<cards.count, id: \.self) { index in
+                CardView(card: cards[index])
+                    .rotationEffect(.degrees(Double(cards.count - 1 - index) * -5))
             }
-            .font(.title)
-            .foregroundStyle(.white)
-            .padding()
         }
-        .frame(width: 300, height: 500)
-        
     }
 }
 
